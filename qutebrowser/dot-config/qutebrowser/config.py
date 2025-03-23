9a190a9bfd92235623cd9405ec9d0f93ce6f0b6d
vimmattu <me@jkur.dev>
2025-03-23 20:40:53 +0000

    update qutebrowser keybinds

--- qutebrowser/dot-config/qutebrowser/config.py
+++ qutebrowser/dot-config/qutebrowser/config.py
@@ -5,3 +5,4 @@ c.fonts.default_family = "IosevkaTerm Nerd Font Mono"
 c.fonts.default_size = "14pt"
 
 config.bind(",l", "spawn --userscript open_in_libproxy")
+config.bind(",m", "spawn --userscript view_in_mpv")

705e2dbe035140f1aff299cbbb27a2c18168cde0
vimmattu <me@jkur.dev>
2025-09-04 18:37:07 +0300

    changes

--- qutebrowser/dot-config/qutebrowser/config.py
+++ qutebrowser/dot-config/qutebrowser/config.py
@@ -3,6 +3,3 @@ config.source("nord-theme.py")
 
 c.fonts.default_family = "IosevkaTerm Nerd Font Mono"
 c.fonts.default_size = "14pt"
-
-config.bind(",l", "spawn --userscript open_in_libproxy")
-config.bind(",m", "spawn --userscript view_in_mpv")
